f525d09cfa7d976a519500fafe5d1ab0d3ae3a08
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-05-13 14:31:19 +0200

    Constructor created

--- src/main/java/org/example/OsmHttpRequest.java
+++ src/main/java/org/example/OsmHttpRequest.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class OsmHttpRequest {
+    public OsmHttpRequest(double minLat, double minLon, double maxLat, double maxLon)
+    {
+        String requestUrl = "https://overpass-api.de/api/interpreter?data=[out:json];way[%27highway%27](" + minLat + ",%20" + minLon + ",%20" + maxLat + ",%20"+ maxLon +");out%20body;%3E;out%20skel%20qt;";
+    }
 }
