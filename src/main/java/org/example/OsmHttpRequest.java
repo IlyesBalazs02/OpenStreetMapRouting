7e5da96a86227449ef9d85c8bf3975e935eff97d
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-05-13 14:33:38 +0200

    SendHttpRequest method

--- src/main/java/org/example/OsmHttpRequest.java
+++ src/main/java/org/example/OsmHttpRequest.java
@@ -1,8 +1,44 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class OsmHttpRequest {
     public OsmHttpRequest(double minLat, double minLon, double maxLat, double maxLon)
     {
         String requestUrl = "https://overpass-api.de/api/interpreter?data=[out:json];way[%27highway%27](" + minLat + ",%20" + minLon + ",%20" + maxLat + ",%20"+ maxLon +");out%20body;%3E;out%20skel%20qt;";
+        Process(requestUrl);
+    }
+
+    public void Process(String requestUrl)
+    {
+        String jsonResponse;
+        try {
+            jsonResponse = sendHttpRequest(requestUrl);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String sendHttpRequest(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            return response.toString();
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
