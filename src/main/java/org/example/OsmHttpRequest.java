e3aa8807140adc89b44518ed1fb9e95ef7a5a9c2
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-05-13 15:17:09 +0200

    Algorithm to process the data is finished

--- src/main/java/org/example/OsmHttpRequest.java
+++ src/main/java/org/example/OsmHttpRequest.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OsmHttpRequest {
     public OsmHttpRequest(double minLat, double minLon, double maxLat, double maxLon)
@@ -27,8 +29,53 @@ public class OsmHttpRequest {
         }
 
         Gson gson = new Gson();
+
+        List<Node> nodeList = new ArrayList<>();
+        List<Way> wayList = new ArrayList<>();
+
+        JsonObject jsonObject = gson.fromJson(jsonResponse, JsonObject.class);
+        JsonArray elements = jsonObject.getAsJsonArray("elements");
+
+        // Process nodes
+        for (JsonElement element : elements) {
+            JsonObject obj = element.getAsJsonObject();
+            String type = obj.get("type").getAsString();
+
+            if (type.equals("node")) {
+                long id = obj.get("id").getAsLong();
+                double lat = obj.get("lat").getAsDouble();
+                double lon = obj.get("lon").getAsDouble();
+
+                nodeList.add(new Node(id, lat, lon));
+            }
+        }
+
+        // Process ways
+        for (JsonElement element : elements) {
+            JsonObject obj = element.getAsJsonObject();
+            String type = obj.get("type").getAsString();
+
+            if (type.equals("way")) {
+                long id = obj.get("id").getAsLong();
+                Way way = new Way(id);
+
+                JsonArray nodesArray = obj.getAsJsonArray("nodes");
+                for (JsonElement nodeElement : nodesArray) {
+                    long nodeId = nodeElement.getAsLong();
+                    for (Node node : nodeList){
+                        if(node.getId() == nodeId){ // need a reference for the coordinates
+                            way.addNode(node);
+                        }
+                    }
+                }
+
+                wayList.add(way);
+            }
+        }
+
     }
 
+    //Send the request and return the osm nodes and ways in json format
     private static String sendHttpRequest(String urlString) throws IOException {
         URL url = new URL(urlString);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
