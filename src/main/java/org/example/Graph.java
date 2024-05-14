2ab19aa10d1f1801c9c3e76f0244bbedfe7adb00
IlyesBalazs <ilyesbalazs02@gmail.com>
2024-05-14 20:56:46 +0200

    Edge and Graphnode classes added

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,4 +1,20 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Graph {
+class GraphNode extends Node{
+    public GraphNode(long id, double lat, double lon)
+    {
+        super(id,lat,lon);
+    }
+    private List<Edge> edges;
+
+    public List<Edge> getNeighbours(){return edges;}
+    public void addEdge(Edge newEdge){ edges.add(newEdge);}
 }
+
+class Edge{
+    private GraphNode destination;
+    private double weight;
+}
+
